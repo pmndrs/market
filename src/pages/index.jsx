a46b93a85fc49e6345c0806215ae2919d549554a
SaraVieira <hey@iamsaravieira.com>
2021-05-02 00:49:26 +0200

    fix donut

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,13 +2,34 @@ import useStore from '@/helpers/store'
 import getAllModels from '@/helpers/getAllModels'
 import Model from '@/components/Model'
 import Layout from '@/components/layout/'
+import { SearchIcon } from '@heroicons/react/solid'
+import { useState } from 'react'
 
 const Page = ({ title, models }) => {
   useStore.setState({ title })
+  const [currentModels, setCurrentModels] = useState(models)
+  const [search, setSearch] = useState('')
   return (
     <Layout>
+      <div>
+        <label htmlFor='search' className='sr-only'>
+          Search for modules
+        </label>
+        <div className='relative mt-1 rounded-md shadow-sm mt-6'>
+          <input
+            type='search'
+            name='search'
+            id='search'
+            className='block w-full pr-10 border-gray-300 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md'
+            placeholder='Search for models'
+          />
+          <div className='absolute inset-y-0 right-0 flex items-center pr-3 pointer-events-none'>
+            <SearchIcon className='w-5 h-5 text-gray-400' aria-hidden='true' />
+          </div>
+        </div>
+      </div>
       <ul className=' mt-10 grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-3 sm:gap-x-6 lg:grid-cols-4 xl:gap-x-8'>
-        {models.map((model) => (
+        {currentModels.map((model) => (
           <Model {...model} key={model.gltf} />
         ))}
       </ul>
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,5 +12,8 @@ module.exports = {
   variants: {
     extend: {},
   },
-  plugins: [require('@tailwindcss/aspect-ratio')],
+  plugins: [
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/forms'),
+  ],
 }
