af6f5728d0dad129847f5eaec545fde38bfe2de8
SaraVieira <hey@iamsaravieira.com>
2021-05-01 17:27:08 +0200

    fix tailwind«

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 module.exports = {
-  // mode: 'jit',
-  purge: ['./src/pages/**/*.js', './src/components/**/*.js'], // remove unused styles in production
-
+  mode: 'jit',
+  purge: ['./src/pages/**/*', './src/components/**/*'],
   darkMode: 'media', // or 'media' or 'class'
   jit: true,
   theme: {
