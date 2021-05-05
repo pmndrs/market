430d6045b897fd2d74a087151dcb772f3b6fc174
SaraVieira <hey@iamsaravieira.com>
2021-05-05 23:08:11 +0200

    last matcaps

--- src/helpers/getMaterial.js
+++ src/helpers/getMaterial.js
@@ -9,23 +9,28 @@ const getMaterial = (name) => {
   if (fs.statSync(newPath).isDirectory()) {
     // eslint-disable-next-line array-callback-return
     fs.readdirSync(newPath).map((filename) => {
+      console.log(material)
       const filePath = path.join(resources, name, filename)
       const fileContents = fs.readFileSync(filePath, 'utf-8')
 
       if (filename.includes('.json')) {
-        material = JSON.parse(fileContents)
+        material = {
+          ...material,
+          ...JSON.parse(fileContents),
+        }
       }
 
       if (filename.includes('render.png')) {
         material.image = `/materials/${name}/${filename}`
       }
-
-      if (filename.includes('.jpg') || filename.includes('.exr')) {
+      if (filename.includes('.jpg')) {
         const { size } = getSize(filePath)
+
         material.size = size
         material.url = `/materials/${name}/${filename}`
       }
     })
+
     return material
   }
 
