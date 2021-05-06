eace203c439523cf1445b7f9c6dcf920ca4d4e61
SaraVieira <hey@iamsaravieira.com>
2021-05-06 19:05:24 +0200

    add categorues to top of item

--- src/components/Model.js
+++ src/components/Model.js
@@ -10,12 +10,17 @@ const Model = (model) => {
         href={`/model/${model.url}`}
       >
         <a>
-          <div className='block w-full overflow-hidden bg-gray-100 rounded-lg group aspect-w-10 aspect-h-7 focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-offset-gray-100 focus-within:ring-indigo-500'>
-            <img
-              src={model.image}
-              alt=''
-              className='object-cover pointer-events-none group-hover:opacity-75'
-            />
+          <div className='relative'>
+            <span className='absolute right-0 z-10 p-2 text-sm text-gray-800 bg-gray-100 rounded-tl-none rounded-tr-lg rounded-br-none rounded-bl-md opacity-85'>
+              {model.info.category}
+            </span>{' '}
+            <div className='block w-full overflow-hidden bg-gray-100 rounded-lg group aspect-w-10 aspect-h-7 focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-offset-gray-100 focus-within:ring-indigo-500'>
+              <img
+                src={model.image}
+                alt=''
+                className='object-cover pointer-events-none group-hover:opacity-75'
+              />
+            </div>
           </div>
           <p className='block mt-2 text-sm font-medium text-gray-900 truncate pointer-events-none'>
             {model.info?.name}
--- src/pages/materials.jsx
+++ src/pages/materials.jsx
@@ -4,6 +4,7 @@ import { useEffect } from 'react'
 import { SearchIcon } from '@heroicons/react/solid'
 import getAllMatcaps from '@/helpers/getAllMatcaps'
 import Link from 'next/link'
+import { shuffle } from '../helpers/shuffle'
 
 const Index = ({ title, materials }) => {
   const { search, currentMaterials, setSearch } = useStore((state) => ({
@@ -12,7 +13,7 @@ const Index = ({ title, materials }) => {
     setSearch: state.setSearch,
   }))
   useEffect(() => {
-    useStore.setState({ currentMaterials: materials })
+    useStore.setState({ currentMaterials: shuffle(materials) })
     useStore.setState({ defaultMaterials: materials })
     useStore.setState({ title })
     // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -49,12 +50,17 @@ const Index = ({ title, materials }) => {
               href={`/material/${material.url}`}
             >
               <a>
-                <div className='block w-full overflow-hidden bg-gray-100 rounded-lg group aspect-w-10 aspect-h-7 focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-offset-gray-100 focus-within:ring-indigo-500'>
-                  <img
-                    src={material.preview}
-                    alt=''
-                    className='object-cover pointer-events-none group-hover:opacity-75'
-                  />
+                <div className='relative'>
+                  <span className='absolute right-0 z-10 p-2 text-sm text-gray-800 bg-gray-100 rounded-tl-none rounded-tr-lg rounded-br-none rounded-bl-md opacity-85'>
+                    {material.info.category}
+                  </span>{' '}
+                  <div className='block w-full overflow-hidden bg-gray-100 rounded-lg group aspect-w-10 aspect-h-7 focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-offset-gray-100 focus-within:ring-indigo-500'>
+                    <img
+                      src={material.preview}
+                      alt=''
+                      className='object-cover pointer-events-none group-hover:opacity-75'
+                    />
+                  </div>
                 </div>
                 <p className='block mt-2 text-sm font-medium text-gray-900 truncate pointer-events-none'>
                   {material.info?.name}
