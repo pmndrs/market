1dfbecd00a4558760061f7fc35ed3532782066cc
Sara Vieira <hey@iamsaravieira.com>
2021-05-09 18:19:37 +0200

    try again

--- src/pages/api/materials/index.js
+++ src/pages/api/materials/index.js
@@ -3,10 +3,12 @@ import path from 'path'
 import getConfig from 'next/config'
 const { serverRuntimeConfig } = getConfig()
 
+const dev = process.env.NODE_ENV !== 'production'
+
+const materialsPath = `${dev ? './public/' : './'}materials/basic-1/info.json`
+
 export default function handler(req, res) {
-  const a = fs.readFileSync(
-    path.join(serverRuntimeConfig.PROJECT_ROOT, './materials/basic-1/info.json')
-  )
+  const a = fs.readFileSync(materialsPath)
 
   res.status(200).json(a)
 }
