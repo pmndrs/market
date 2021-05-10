4f0fb7668f39d30b57992e55be4e4a34c51a72a4
Sara Vieira <hey@iamsaravieira.com>
2021-05-10 17:59:27 +0200

    add cc-by license

--- src/helpers/constants/licenses.js
+++ src/helpers/constants/licenses.js
@@ -1,6 +1,10 @@
 export const licenses = {
   1: {
-    link: "https://creativecommons.org/share-your-work/public-domain/cc0/'",
+    link: 'https://creativecommons.org/share-your-work/public-domain/cc0/',
     name: 'CC0',
+  },
+  2: {
+    link: 'https://creativecommons.org/licenses/by/2.0/',
+    name: 'CC-BY',
   },
 }
