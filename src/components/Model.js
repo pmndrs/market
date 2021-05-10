77bd612f78d9c8e162a75a8b095d520a0b63fd0b
Sara Vieira <hey@iamsaravieira.com>
2021-05-10 14:55:35 +0200

    create favorite button component

--- src/components/Model.js
+++ src/components/Model.js
@@ -1,11 +1,9 @@
 import React from 'react'
 import Link from 'next/link'
 import Tippy from '@tippyjs/react'
-import useStore from '@/helpers/store'
+import FavoriteButton from './FavoriteButton'
 
 const Model = (model) => {
-  const user = useStore((state) => state.user)
-  const toggleFavorite = useStore((state) => state.toggleFavorite)
   return (
     <li key={model.image} className='relative'>
       <Link
@@ -29,44 +27,7 @@ const Model = (model) => {
             <p className='block mt-2 text-sm font-medium text-gray-900 truncate pointer-events-none'>
               {model.info?.name}
             </p>
-            <button
-              className='z-90'
-              onClick={async (e) => {
-                e.preventDefault()
-                toggleFavorite(model, 'models')
-              }}
-            >
-              {user?.profile?.favorites &&
-              user.profile.favorites.includes(`models/${model.url}`) ? (
-                <svg
-                  xmlns='http://www.w3.org/2000/svg'
-                  className='h-5 w-5 text-red-600'
-                  viewBox='0 0 20 20'
-                  fill='currentColor'
-                >
-                  <path
-                    fillRule='evenodd'
-                    d='M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z'
-                    clipRule='evenodd'
-                  />
-                </svg>
-              ) : (
-                <svg
-                  xmlns='http://www.w3.org/2000/svg'
-                  className='w-5 h-5 text-red-600'
-                  fill='none'
-                  viewBox='0 0 24 24'
-                  stroke='currentColor'
-                >
-                  <path
-                    strokeLinecap='round'
-                    strokeLinejoin='round'
-                    strokeWidth={2}
-                    d='M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z'
-                  />
-                </svg>
-              )}
-            </button>
+            <FavoriteButton type='models' asset={model} />
           </div>
           <p className='block text-sm font-medium text-gray-500'>
             {model.highPoly && (
