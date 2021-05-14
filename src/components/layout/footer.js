47428d911589bc09dce5b4e71ec9a5fecd71fa7e
Sara Vieira <hey@iamsaravieira.com>
2021-05-14 19:53:32 +0200

    fix a11y issue

--- src/components/layout/footer.js
+++ src/components/layout/footer.js
@@ -85,7 +85,7 @@ const Footer = () => {
           ))}
         </div>
         <div className='mt-8 md:mt-0 md:order-1'>
-          <p className='text-base text-center text-gray-400'>
+          <p className='text-base text-center text-gray-500'>
             &copy; {new Date().getFullYear()} pmndrs.
           </p>
         </div>
