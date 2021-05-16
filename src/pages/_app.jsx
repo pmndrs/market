c80c9eb2eb2a30185cc72b69eb20c0777f337c66
Sara Vieira <hey@iamsaravieira.com>
2021-05-16 11:39:02 +0200

    seo fixes

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -5,6 +5,7 @@ import { supabase } from '../helpers/initSupabase'
 import useStore from '@/helpers/store'
 import * as Fathom from 'fathom-client'
 import { useRouter } from 'next/router'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
   const user = supabase.auth.user()
@@ -108,12 +109,75 @@ function MyApp({ Component, pageProps }) {
   }, [])
 
   return (
-    <Component
-      user={user}
-      session={session}
-      authView={authView}
-      {...pageProps}
-    />
+    <>
+      <Head>
+        <link rel='amphtml' href={'https://market.pmnd.rs' + router.asPath} />
+        <link rel='canonical' href={'https://market.pmnd.rs' + router.asPath} />
+        <meta name='googlebot' content='follow, index, noarchive' />
+        <meta name='robots' content='follow, index, noarchive' />
+        <meta name='viewport' content='initial-scale=1,width=device-width' />
+
+        <link rel='apple-touch-icon' href='/apple-touch-icon.png' />
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='16x16'
+          href='/favicon-16x16.png'
+        />
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='32x32'
+          href='/favicon-32x32.png'
+        />
+
+        <link rel='manifest' href='/site.webmanifest' />
+        <meta name='apple-mobile-web-app-capable' content='yes' />
+        <meta name='apple-mobile-web-app-status-bar-style' content='#000000' />
+        <meta name='apple-mobile-web-app-title' content='PMNDRS Market' />
+        <meta name='application-name' content='PMNDRS Market' />
+        <meta name='mobile-web-app-capable' content='yes' />
+        <meta name='msapplication-config' content='/browserconfig.xml' />
+        <meta name='msapplication-navbutton-color' content='#000000' />
+        <meta name='msapplication-starturl' content='https://market.pmnd.rs/' />
+        <meta name='msapplication-tilecolor' content='#000000' />
+        <meta name='msapplication-tileimage' content='/mstile-144x144.png' />
+        <meta name='msapplication-tooltip' content='PMNDRS Market' />
+        <meta name='title' content='pmndrs market' />
+        <meta
+          name='description'
+          content="Your home for downloading web-ready 3D assets. Download CC0 models, textures and HDRI's that are web-ready."
+        />
+
+        <meta property='og:type' content='website' />
+        <meta property='og:url' content='http://market.pmnd.rs/' />
+        <meta property='og:title' content='pmndrs market' />
+        <meta
+          property='og:description'
+          content="Your home for downloading web-ready 3D assets. Download CC0 models, textures and HDRI's that are web-ready."
+        />
+        <meta property='og:image' content='http://market.pmnd.rs/share.png' />
+
+        <meta property='twitter:card' content='summary_large_image' />
+        <meta property='twitter:url' content='http://market.pmnd.rs/' />
+        <meta property='twitter:title' content='pmndrs market' />
+        <meta
+          property='twitter:description'
+          content="Your home for downloading web-ready 3D assets. Download CC0 models, textures and HDRI's that are web-ready."
+        />
+        <meta
+          property='twitter:image'
+          content='http://market.pmnd.rs/share.png'
+        />
+      </Head>
+
+      <Component
+        user={user}
+        session={session}
+        authView={authView}
+        {...pageProps}
+      />
+    </>
   )
 }
 export default MyApp
