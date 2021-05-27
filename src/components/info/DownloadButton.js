4ad63eee92ec1771b5fa30bc189cc25d8eb037a8
njm222 <nathan.john.menezes@gmail.com>
2021-05-27 12:30:52 +0530

    add missing margin to a tag

--- src/components/info/DownloadButton.js
+++ src/components/info/DownloadButton.js
@@ -2,7 +2,7 @@ const DownloadButton = ({ children, onClick, ...props }) => {
   if (props.href) {
     return (
       <a
-        className='block w-full py-2 text-center text-white bg-gray-800 disabled:opacity-75 disabled:cursor-auto'
+        className='block w-full py-2 mt-4 text-center text-white bg-gray-800 disabled:opacity-75 disabled:cursor-auto'
         download
         {...props}
       >
