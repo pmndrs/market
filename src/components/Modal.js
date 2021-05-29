985e228058aed088f16e83be58ed59df5beae1b5
Sara Vieira <hey@iamsaravieira.com>
2021-05-29 16:21:23 +0200

    delete comment

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,4 +1,3 @@
-/* This example requires Tailwind CSS v2.0+ */
 import { Fragment, useState } from 'react'
 import { Dialog, Transition } from '@headlessui/react'
 import { ExclamationIcon, XIcon } from '@heroicons/react/outline'
