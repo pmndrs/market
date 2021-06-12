0de635208cecd51ae7ee17370309925717505b0a
Alaric Baraou <alaric.baraou@gmail.com>
2021-06-12 11:22:52 +0200

    handle animations

--- src/components/canvas/Model.js
+++ src/components/canvas/Model.js
@@ -1,7 +1,8 @@
-import { Canvas } from '@react-three/fiber'
+import { Canvas, useFrame } from '@react-three/fiber'
 import { OrbitControls, Stage, useGLTF } from '@react-three/drei'
 import { Suspense, useRef, useLayoutEffect, useEffect } from 'react'
 import { useControls } from 'leva'
+import { AnimationMixer } from 'three'
 import { lightControls, defaultControls } from './controls'
 
 const Model = ({ file }) => {
@@ -12,21 +13,51 @@ const Model = ({ file }) => {
     ...lightControls,
   })
 
-  const { scene } = useGLTF(file)
+  const { animations,scene } = useGLTF(file)
+  const content = scene || scenes[0];
+  const mixer = new AnimationMixer( content );
+  const animationsClip = []
+  let defaultAnimationsControls = {}
+
+  for (let a of animations){
+    let action = mixer.clipAction(a);
+    animationsClip[a.name] = action
+    defaultAnimationsControls[a.name] = false
+  }
+  const [controlsAnim,setControlsAnim] = useControls('Animations',()=>(
+    defaultAnimationsControls
+  ))
+
+  for(let clipName in animationsClip){
+    if(controlsAnim[clipName]){
+      animationsClip[clipName].play();
+    }else{
+      animationsClip[clipName].stop();
+    }
+  }
 
   useLayoutEffect(() => {
     void scene.traverse(
       (obj) => obj.isMesh && (obj.castShadow = obj.receiveShadow = true)
     )
   }, [scene])
-
+  
   useEffect(() => {
     scene.traverse((obj) => {
       if (obj.isMesh && obj.material) {
         obj.material.wireframe = controls.wireframe
       }
     })
+
+    if(animations[0]){
+      defaultAnimationsControls[animations[0].name] = true
+    }
+    setControlsAnim(defaultAnimationsControls)
   }, [scene, controls.wireframe])
+  
+  useFrame((state, delta) => {
+    mixer.update(delta)
+  })
 
   return (
     <>
