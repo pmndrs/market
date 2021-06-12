da94645bc34e18af8a774ff9fe7c4fa1fd985f51
Sara Vieira <hey@iamsaravieira.com>
2021-06-12 21:14:45 +0200

    fixes hdris; closes #66

--- src/components/CreatorPage.js
+++ src/components/CreatorPage.js
@@ -28,27 +28,29 @@ const CreatorPage = ({ title, creator }) => {
           <h1 className={`text-3xl font-bold leading-tight text-gray-900 `}>
             {title}
           </h1>
-          <a
-            href={creator.link}
-            target='blank'
-            className='flex items-center mt-1 text-sm font-bold'
-          >
-            <svg
-              xmlns='http://www.w3.org/2000/svg'
-              className='w-5 h-5 mr-1 text-gray-900'
-              fill='none'
-              viewBox='0 0 24 24'
-              stroke='currentColor'
+          {creator.link && (
+            <a
+              href={creator.link}
+              target='blank'
+              className='flex items-center mt-1 text-sm font-bold'
             >
-              <path
-                strokeLinecap='round'
-                strokeLinejoin='round'
-                strokeWidth={2}
-                d='M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z'
-              />
-            </svg>
-            Visit Website
-          </a>
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                className='w-5 h-5 mr-1 text-gray-900'
+                fill='none'
+                viewBox='0 0 24 24'
+                stroke='currentColor'
+              >
+                <path
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  strokeWidth={2}
+                  d='M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z'
+                />
+              </svg>
+              Visit Website
+            </a>
+          )}
           {creator.donateLink && (
             <a
               href={creator.donateLink}
